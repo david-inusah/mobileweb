44425c452274683d718bf956fe97b86d702dbd22
david_inusah <david.inusah@outlook.com>
2016-10-20 19:29:33 +0100

    adding geolocation and camera

--- scripts/index.js
+++ scripts/index.js
@@ -7,32 +7,33 @@
     function onDeviceReady() {
         pictureSource = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
-
+}
         document.getElementById("capturePhoto").onclick = function() {
+            alert("I'm here");
             navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
                 quality: 50,
 
                 destinationType: destinationType.DATA_URL
             });
         }
+        
         document.getElementById("geolocator").onclick = function() {
-            navigator.geolocation.getCurrentPosition { onSuccess, onError, enableHighAccuracy: true }
-        }
-    };
+            navigator.geolocation.getCurrentPosition (onSuccess, onError, {enableHighAccuracy: true}) 
+        };
+    
 
     function onSuccess(position) {
 
         alert(
             'Latitude: ' + position.coords.latitude + '\n' +
-            'Longitude: ' + position.coords.longitude + '\n' +
+            'Longitude: ' + position.coords.longitude + '\n'
         );
     }
 
     function onError(error) {
         alert(
             'code: ' + error.code + '\n' +
-            'message: ' + error.message + '\n' +
-        );
+            'message: ' + error.message + '\n');
     }
 
     function onPhotoDataSuccess(imageData) {
