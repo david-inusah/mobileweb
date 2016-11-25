fdae2dd9fe1c6c4765f186848bc2710b01a6d289
david_inusah <david.inusah@outlook.com>
2016-11-25 20:14:31 +0000

    fixing bugs

--- scripts/index.js
+++ scripts/index.js
@@ -7,38 +7,39 @@
     function onDeviceReady() {
         pictureSource = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
-}
-        document.getElementById("capturePhoto").onclick = function() {
-         //   alert("I'm here");
-            navigator.camera.getPicture(onPhotoDataSuccess, onFail,{
+
+        document.getElementById("capturePhoto").onclick = function () {
+            //   alert("I'm here");
+            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
                 quality: 50,
                 destinationType: destinationType.DATA_URL
             });
         }
 
-        document.getElementById("geolocator").onclick = function() {
-            navigator.geolocation.getCurrentPosition (onSuccess, onError, {enableHighAccuracy: true
-            }); 
+        document.getElementById("geolocator").onclick = function () {
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+                enableHighAccuracy: true
+            });
         }
-    $(function () {
-        document.getElementById("barcode").addEventListener("click", function () {
-            cordova.plugins.barcodeScanner.scan(
-                function (result) {
-                    alert("Information: " + result.text + "\n" +
-                        "Format: " + result.format + "\n");
-                },
-                function (error) {
-                    alert(error);
-                }
-            );
+        $(function () {
+            document.getElementById("barcode").addEventListener("click", function () {
+                cordova.plugins.barcodeScanner.scan(
+                    function (result) {
+                        alert("Information: " + result.text + "\n" +
+                            "Format: " + result.format + "\n");
+                    },
+                    function (error) {
+                        alert(error);
+                    }
+                );
+            });
         });
-    });
-        
+    };
     function onSuccess(position) {
 
         var latitude = position.coords.latitude,
             longitude = position.coords.longitude,
-            cordinates = latitude + longitude;
+            cordinates = latitude +','+ longitude;
         alert('Latitude: ' + latitude + '\n' + 'Longitude: ' + longitude + '\n');
         document.getElementById('google-map').setAttribute('src, http://maps.google.co.uk?q=' + cordinates + '&z=60&output=embed')
 
@@ -65,5 +66,4 @@
         alert('Failed because: ' + message);
 
     }
-
 })();
