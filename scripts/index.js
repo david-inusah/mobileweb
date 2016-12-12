289c284153ca1524539847de7fd8862efcf3a399
david_inusah <david.inusah@outlook.com>
2016-12-12 10:31:52 +0000

    adding fix

--- scripts/index.js
+++ scripts/index.js
@@ -16,40 +16,40 @@
             });
         }
 
-        // document.getElementById("geolocator").onclick = function () {
-        //     navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-        //         enableHighAccuracy: true
-        //     });
-        // }
-        // $(function () {
-        //     document.getElementById("barcode").addEventListener("click", function () {
-        //         cordova.plugins.barcodeScanner.scan(
-        //             function (result) {
-        //                 alert("Information: " + result.text + "\n" +
-        //                     "Format: " + result.format + "\n");
-        //             },
-        //             function (error) {
-        //                 alert(error);
-        //             }
-        //         );
-        //     });
-        // });
+        document.getElementById("geolocator").onclick = function () {
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+                enableHighAccuracy: true
+            });
+        }
+        $(function () {
+            document.getElementById("barcode").addEventListener("click", function () {
+                cordova.plugins.barcodeScanner.scan(
+                    function (result) {
+                        alert("Information: " + result.text + "\n" +
+                            "Format: " + result.format + "\n");
+                    },
+                    function (error) {
+                        alert(error);
+                    }
+                );
+            });
+        });
     };
-    // function onSuccess(position) {
+    function onSuccess(position) {
 
-    //     var latitude = position.coords.latitude,
-    //         longitude = position.coords.longitude,
-    //         cordinates = latitude +','+ longitude;
-    //     alert('Latitude: ' + latitude + '\n' + 'Longitude: ' + longitude + '\n');
-    //     document.getElementById('google-map').setAttribute('src, http://maps.google.co.uk?q=' + cordinates + '&z=60&output=embed')
+        var latitude = position.coords.latitude,
+            longitude = position.coords.longitude,
+            cordinates = latitude +','+ longitude;
+        alert('Latitude: ' + latitude + '\n' + 'Longitude: ' + longitude + '\n');
+        // document.getElementById('google-map').setAttribute('src, http://maps.google.co.uk?q=' + cordinates + '&z=60&output=embed')
 
-    // }
+    }
 
-    // function onError(error) {
-    //     alert(
-    //         'code: ' + error.code + '\n' +
-    //         'message: ' + error.message + '\n');
-    // }
+    function onError(error) {
+        alert(
+            'code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
+    }
 
     function onPhotoDataSuccess(imageData) {
 
